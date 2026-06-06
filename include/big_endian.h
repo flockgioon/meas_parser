49b520e80553b64f807f1f56b97d4dd3cc483626
flockgioon <agco82@gmail.com>
2026-06-06 15:08:58 +0800

    module big_endian: update function big_endian_write_* signature and implement

--- include/big_endian.h
+++ include/big_endian.h
@@ -6,6 +6,6 @@ uint16_t big_endian_uint16(const uint8_t *data);
 uint32_t big_endian_uint32(const uint8_t *data);
 uint64_t big_endian_uint64(const uint8_t *data);
 
-void big_endian_write_uint16(const uint8_t *data, uint16_t v);
-void big_endian_write_uint32(const uint8_t *data, uint32_t v);
-void big_endian_write_uint64(const uint8_t *data, uint64_t v);
+void big_endian_write_uint16(uint8_t *data, uint16_t v);
+void big_endian_write_uint32(uint8_t *data, uint32_t v);
+void big_endian_write_uint64(uint8_t *data, uint64_t v);
--- src/big_endian.c
+++ src/big_endian.c
@@ -23,3 +23,25 @@ uint64_t big_endian_uint64(const uint8_t *p) {
            ((uint64_t)p[6] << 8) |
            ((uint64_t)p[7]);
 }
+
+void big_endian_write_uint16(uint8_t *data, uint16_t v) {
+    data[0] = (uint8_t)(v >> 8);
+    data[1] = (uint8_t)v;
+}
+
+void big_endian_write_uint32(uint8_t *data, uint32_t v) {
+    data[0] = (uint8_t)(v >> 24);
+    data[1] = (uint8_t)(v >> 16);
+    data[2] = (uint8_t)(v >> 8);
+    data[3] = (uint8_t)v;
+}
+void big_endian_write_uint64(uint8_t *data, uint64_t v) {
+    data[0] = (uint8_t)(v >> 56);
+    data[1] = (uint8_t)(v >> 48);
+    data[2] = (uint8_t)(v >> 40);
+    data[3] = (uint8_t)(v >> 32);
+    data[4] = (uint8_t)(v >> 24);
+    data[5] = (uint8_t)(v >> 16);
+    data[6] = (uint8_t)(v >> 8);
+    data[7] = (uint8_t)v;
+}
