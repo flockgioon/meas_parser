ab3357fe715e82455ffca1c16ec7d703f976c7a8
flockgioon <flockgioon@gmail.com>
2026-06-13 20:49:07 +0800

    module portable: create and implement (for Linux compatibility)

---
+++ include/portable.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <errno.h>
+
+int portable_fopen(FILE **file, const char *filepath, const char *mode);
---
+++ src/portable.c
@@ -0,0 +1,10 @@
+#include "portable.h"
+
+int portable_fopen(FILE **file, const char *filepath, const char *mode) {
+#ifdef _MSC_VER
+    return (int)fopen_s(file, filepath, mode);
+#else
+    *file = fopen(filepath, mode);
+    return file == NULL ? errno : 0;
+#endif
+}
